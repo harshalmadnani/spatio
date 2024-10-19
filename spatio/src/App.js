fea47bc095cc2ebdaaff8223e799721f306cd33d
Chinmay Lohani <chinmaylohani27@gmail.com>
2024-10-19 06:53:14 -0400

    removed privy

--- spatio/src/App.js
+++ spatio/src/App.js
@@ -1,22 +1,21 @@
 import "./App.css";
-import { usePrivy } from "@privy-io/react-auth";
+// import { usePrivy } from "@privy-io/react-auth";
 import ChatInterface from "./ai.js";
 
 function App() {
-
-  const { ready, authenticated, user, login, logout } = usePrivy();
-  if (!ready) {
-    return null;
-  }
+  // const { ready, authenticated, user, login, logout } = usePrivy();
+  // if (!ready) {
+  //   return null;
+  // }
 
   return (
     <div className="App">
       <header className="App-header">
-      {ready && authenticated ? (
-          <ChatInterface />
-        ) : (
-          <button onClick={login} style={{padding: "12px", backgroundColor: "#069478", color: "#FFF", border: "none", borderRadius: "6px" }}>Log In</button>
-        )}
+        {/* {ready && authenticated ? (*/}
+        <ChatInterface />
+        {/* ) : (
+          <button onClick={login} style={{padding: "12px", backgroundColor: "#069478", color: "#FFF", border: "none", borderRadius: "6px" }}>Log In</button> */}
+        {/* )} */}
       </header>
     </div>
   );

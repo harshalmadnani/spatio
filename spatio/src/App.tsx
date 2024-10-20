5c76e45bc978afe21bcf3e3c176e0ac5a97f9ad7
Xade <development@xade.finance>
2024-10-20 07:02:54 -0700

    Update App.tsx

--- spatio/src/App.tsx
+++ spatio/src/App.tsx
@@ -9,37 +9,35 @@ function App() {
 
   return (
     <div className="App">
-      {!isLoggedIn && (
-        <>
-          <video autoPlay loop muted className="background-video">
-            <source src={require('./1.mp4')} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          <div className="content">
-            <header className="App-header">
-              <img 
-                src={require('./SPATIO.png')} 
-                alt="spatio AI Logo" 
-                style={{ width: '200px', height: 'auto' }}
-              />
-              <p style={{
-                fontFamily: 'monospace',
-                marginTop: '10%',
-                marginBottom: '20%',
-                textAlign: 'center',
-              }}>
-                Your personal crypto trading and research assistant
-              </p>
-            </header>
-          </div>
-        </>
-      )}
-      <DynamicWidget />
-      {isLoggedIn && (
-        <>
-          <ChatInterface />
-        </>
-      )}
+      <video autoPlay loop muted className="background-video">
+        <source src={require('./1.mp4')} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="content">
+        <header className="App-header">
+          <img 
+            src={require('./SPATIO.png')} 
+            alt="spatio AI Logo" 
+            style={{ width: '200px', height: 'auto' }}
+          />
+          <p style={{
+            fontFamily: 'monospace',
+            marginTop: '10%',
+            marginBottom: '20%',
+            textAlign: 'center',
+          }}>
+            Your personal crypto trading and research assistant
+          </p>
+          <DynamicWidget />
+
+          {}
+          {isLoggedIn && (
+            <>
+              <ChatInterface />
+            </>
+          )}
+        </header>
+      </div>
     </div>
   );
 }
